df194d7781a683b8d159d7c9621c87622d717111
Dariia Porechna <35382599+dariolina@users.noreply.github.com>
2026-01-17 18:23:22 +0100

    try on wrong message

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,8 @@ fn main() {
     // Verify signature
     let is_valid = verify(message_input.as_bytes(), &signature, &public_key);
 
+    let is_forged = verify(&[1, 2, 3], &signature, &public_key);
+
     println!("Private Key: ({}, {})", private_key.k, private_key.omega);
     println!(
         "Private Key (hex): (0x{}, 0x{})",
@@ -53,4 +55,5 @@ fn main() {
     println!("Signature: ({}, {}, {}, {}, {})", signature.sigma_1, signature.sigma_2, signature.sigma_3, signature.sigma_4, signature.sigma_5);
     println!("Signature (hex): (0x{}, 0x{}, 0x{}, 0x{}, 0x{})", hex::encode(signature.sigma_1.into_bigint().to_bytes_be()), hex::encode(signature.sigma_2.into_bigint().to_bytes_be()), hex::encode(signature.sigma_3.into_bigint().to_bytes_be()), hex::encode(signature.sigma_4.into_bigint().to_bytes_be()), hex::encode(signature.sigma_5.into_bigint().to_bytes_be()));
     println!("Is valid: {}", is_valid);
+    println!("Invalid for wrong message [1, 2, 3]: {}", is_forged);
 }
